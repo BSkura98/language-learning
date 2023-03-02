be49e9d94ca13d36aef0b741d95eafb79eca0b1b
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2023-03-02 23:15:17 +0100

    Configure translations

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,28 +1,21 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'standard-with-typescript',
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended', 'standard-with-typescript'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  plugins: [
-    'react',
-    'prettier'
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
-    '@typescript-eslint/semi': [2, "always"],
-    "@typescript-eslint/space-before-function-paren": [0],
-    "@typescript-eslint/triple-slash-reference": [0],
-    'prettier/prettier': 'warn'
-  }
-}
+    '@typescript-eslint/semi': [2, 'always'],
+    '@typescript-eslint/space-before-function-paren': [0],
+    '@typescript-eslint/triple-slash-reference': [0],
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/comma-dangle': [0],
+  },
+};
