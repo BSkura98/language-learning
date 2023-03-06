e25937c6787c043ad21aa6954d24b9bd15e6d6bd
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2023-03-06 20:55:41 +0100

    Create login page (#7)
    * Add routes
    
    * Create log in page
    
    * Remove unnecessary lines

--- web/src/pages/LogIn/index.tsx
+++ web/src/pages/LogIn/index.tsx
@@ -1,8 +1,30 @@
 import React, { memo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Button, TextField, Typography } from '@mui/material';
+import { Helmet } from 'react-helmet-async';
+
+import { LoginForm, Wrapper } from './styled';
+import { PasswordTextField } from '../../components/PasswordTextField';
 
 export const LogIn = memo((): JSX.Element => {
-  const { t } = useTranslation();
+  const { t } = useTranslation('translation', { keyPrefix: 'pages.logIn' });
 
-  return <h2>{t('pages.logIn.logIn')}</h2>;
+  return (
+    <>
+      <Helmet>
+        <title>{t('pageTitle')}</title>
+        <meta name="description" content={`${t('pageDescription')}`} />
+      </Helmet>
+      <Wrapper>
+        <LoginForm>
+          <Typography variant="h5" gutterBottom>
+            {t('title')}
+          </Typography>
+          <TextField id="outlined-basic" label={t('emailPlaceholder')} variant="outlined" />
+          <PasswordTextField />
+          <Button variant="contained">{t('logInButton')}</Button>
+        </LoginForm>
+      </Wrapper>
+    </>
+  );
 });
