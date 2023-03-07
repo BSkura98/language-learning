64047c84902f1a6782554895a5a9939cec84f17b
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2023-03-07 18:03:57 +0100

    Add logic to log in page

--- web/src/components/PasswordTextField/index.tsx
+++ web/src/components/PasswordTextField/index.tsx
@@ -3,8 +3,9 @@ import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput } fr
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { useTranslation } from 'react-i18next';
+import { type OutlinedInputProps } from '@mui/material/OutlinedInput/OutlinedInput';
 
-export const PasswordTextField = memo(() => {
+export const PasswordTextField = memo((props: OutlinedInputProps) => {
   const [showPassword, setShowPassword] = useState(false);
   const { t } = useTranslation('translation', { keyPrefix: 'components.passwordTextField' });
 
@@ -16,6 +17,7 @@ export const PasswordTextField = memo(() => {
     <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
       <InputLabel htmlFor="outlined-adornment-password">{t('password')}</InputLabel>
       <OutlinedInput
+        {...props}
         id="outlined-adornment-password"
         type={showPassword ? 'text' : 'password'}
         endAdornment={
--- web/src/pages/LogIn/index.tsx
+++ web/src/pages/LogIn/index.tsx
@@ -1,13 +1,23 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button, TextField, Typography } from '@mui/material';
+import { Alert, TextField, Typography } from '@mui/material';
 import { Helmet } from 'react-helmet-async';
+import { useMutation } from 'react-query';
+import { Auth } from 'aws-amplify';
+import { type CognitoUser } from 'amazon-cognito-identity-js';
+import { LoadingButton } from '@mui/lab';
 
 import { LoginForm, Wrapper } from './styled';
 import { PasswordTextField } from '../../components/PasswordTextField';
 
 export const LogIn = memo((): JSX.Element => {
   const { t } = useTranslation('translation', { keyPrefix: 'pages.logIn' });
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const mutation = useMutation<CognitoUser | any, Error>(async () => {
+    return await Auth.signIn(email, password);
+  });
 
   return (
     <>
@@ -16,13 +26,35 @@ export const LogIn = memo((): JSX.Element => {
         <meta name="description" content={`${t('pageDescription')}`} />
       </Helmet>
       <Wrapper>
-        <LoginForm>
+        <LoginForm
+          onSubmit={e => {
+            e.preventDefault();
+            mutation.mutate();
+          }}
+        >
           <Typography variant="h5" gutterBottom>
             {t('title')}
           </Typography>
-          <TextField id="outlined-basic" label={t('emailPlaceholder')} variant="outlined" />
-          <PasswordTextField />
-          <Button variant="contained">{t('logInButton')}</Button>
+          {mutation.isSuccess && <p>Success!</p>}
+          <TextField
+            id="outlined-basic"
+            label={t('emailPlaceholder')}
+            variant="outlined"
+            onChange={e => {
+              setEmail(e.target.value);
+            }}
+            value={email}
+          />
+          <PasswordTextField
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+            value={password}
+          />
+          {mutation.isError && <Alert severity="error">{mutation.error.message}</Alert>}
+          <LoadingButton variant="contained" loading={mutation.isLoading} loadingPosition="start" type="submit">
+            {t('logInButton')}
+          </LoadingButton>
         </LoginForm>
       </Wrapper>
     </>
