9212ddb3aa88a778d7361a40f0cf97e81dc29a9b
Bartłomiej Skura <bartek33s@wp.pl>
2023-05-02 08:39:46 +0200

    Add getRepetitions endpoint

---
+++ api/src/service/getRepetitions/service.ts
@@ -0,0 +1,27 @@
+import { APIGatewayProxyResult } from 'aws-lambda';
+
+import { getRepetitionRepository } from '../../repository/repetition.repository';
+import { GetRepetitionsRequest } from './request';
+import { getSort } from '../../utils/getSort';
+import { endOfDay } from 'date-fns';
+
+export const getRepetitionsService = async (
+  requestParameters: GetRepetitionsRequest
+): Promise<APIGatewayProxyResult> => {
+  const repetitionRepository = await getRepetitionRepository();
+  const { sortBy, sortType } = getSort('repetition', requestParameters.sort);
+
+  const repetitions = repetitionRepository
+    .createQueryBuilder('repetition')
+    .where('repetition.userId = :userId', { userId: requestParameters.userId })
+    .where('repetition.nextRepetitionDate >= :startDate', { startDate: requestParameters.startDate })
+    .where('repetition.nextRepetitionDate <= :endDate', {
+      endDate: requestParameters.endDate ? endOfDay(requestParameters.endDate) : ''
+    })
+    .where('repetition.sourceLanguage = :sourceLanguage', { sourceLanguage: requestParameters.sourceLanguage })
+    .where('repetition.targetLanguage = :targetLanguage', { targetLanguage: requestParameters.targetLanguage })
+    .orderBy(sortBy, sortType)
+    .getMany();
+
+  return repetitions;
+};
